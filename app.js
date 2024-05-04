562264060f289bff1603ebd975ad37b8b0e4e474
j0y0nt <4onsocial@gmail.com>
2024-05-04 18:49:14 +0530

    Added dummy API endpoints.

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+
+var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
@@ -22,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Handle all API call.
+app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
