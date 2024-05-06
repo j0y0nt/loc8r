f9d34590a42926d6d624f285d5a6cf448a7eb0a9
j0y0nt <4onsocial@gmail.com>
2024-05-06 17:31:07 +0530

    Added error page.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
 var renderHomepage = function (req, res, responseBody) {
     var message;
 
-    if(!(responseBody instanceof Array)) {
+    if (!(responseBody instanceof Array)) {
         message = "API lookup error";
         responseBody = [];
     } else {
@@ -52,12 +52,12 @@ module.exports.homelist = function (req, res) {
         function (err, response, body) {
             var i, data;
             data = body;
-            if(response.statusCode === 200 && data.length) {
+            if (response.statusCode === 200 && data.length) {
                 for (i = 0; i < data.length; i++) {
                     data[i].distance = _formatDistance(data[i].distance);
                 }
             }
-            
+
             renderHomepage(req, res, data);
         }
     );
@@ -78,7 +78,7 @@ var _formatDistance = function (distance) {
 var renderDetailPage = function (req, res, locDetail) {
     res.render('location-info', {
         title: locDetail.name,
-        pageHeader: {title: locDetail.name},
+        pageHeader: { title: locDetail.name },
         sidebar: {
             context: 'Costly is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
             callToAction: 'If you\'ve been and you like it - or if you don\'t -please leave a review to help other people just like you.'
@@ -99,16 +99,36 @@ module.exports.locationInfo = function (req, res) {
     };
     request(
         requestOptions,
-        function(err, response, body) {
-            var data = body;
-            data.coords = {
-                lng : body.coords[0],
-                lat : body.coords[1]
-            };
-            renderDetailPage(req, res, data);
+        function (err, response, body) {
+            if (response.statusCode === 200) {
+                var data = body;
+                data.coords = {
+                    lng: body.coords[0],
+                    lat: body.coords[1]
+                };
+                renderDetailPage(req, res, data);
+            } else {
+                _showError(req, res, response.statusCode);
+            }
         }
     );
 };
+
+var _showError = function (req, res, status) {
+    var title, content;
+    if (status === 404) {
+    title = "404, page not found";
+    content = "Oh dear. Looks like we can't find this page. Sorry.";
+    } else {
+    title = status + ", something's gone wrong";
+    content = "Something, somewhere, has gone just a little bit wrong.";
+    }
+    res.status(status);
+    res.render('generic-text', {
+    title : title,
+    content : content
+    });
+    };
 
 /* GET "Add review page" */
 module.exports.addReview = function (req, res) {
