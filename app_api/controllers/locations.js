e9cf06ab0fafbc3b1da15d4b7b1b8549a0660e00
j0y0nt <4onsocial@gmail.com>
2024-05-06 15:54:21 +0530

    Link Frontend with backend API call for locations.

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,32 +1,40 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
-var sendJsonResponse = function(res, status, content) {
+var sendJsonResponse = function (res, status, content) {
     res.status(status);
     res.json(content);
 };
 
-module.exports.locationsListByDistance = function (req, res) {
-    sendJsonResponse(res, 200, {"status": "success"});
+module.exports.locationsListByDistance = async function (req, res) {
+
+    const locations = await Loc.find().exec();
+    
+    if (locations == null) {
+        sendJsonResponse(res, 404, "locations not found.");
+    } else {
+        sendJsonResponse(res, 200, locations);
+    }
+    //sendJsonResponse(res, 200, {"status": "success"});
 };
 
 module.exports.locationsCreate = function (req, res) {
-    sendJsonResponse(res, 200, {"status": "success"});
-};  
+    sendJsonResponse(res, 200, { "status": "success" });
+};
 
-module.exports.locationsReadOne =  async function (req, res) {
+module.exports.locationsReadOne = async function (req, res) {
 
-    if(!req.param && !req.param.locationid) {
+    if (!req.param && !req.param.locationid) {
         sendJsonResponse(res, 404, {
             "message": "No LocationId in request."
         });
-        
+
     } else {
         const location = await Loc.findById(req.params.locationid).exec();
 
-        if(location == null) {
+        if (location == null) {
             sendJsonResponse(res, 404, "location id not found.");
-        }  else {
+        } else {
             sendJsonResponse(res, 200, location);
         }
     }
@@ -42,20 +50,20 @@ module.exports.locationsUpdateOne = function (req, res) {
 
     const location = Loc.findById(req.params.locationid).select('-reviews -rating').exec();
 
-    if(!location) {
-        sendJsonResponse(res, 404, {"message": "locationid not found"});
+    if (!location) {
+        sendJsonResponse(res, 404, { "message": "locationid not found" });
         return;
     } else {
         location.name = req.body.name;
         location.address = req.body.address;
-        location.facilities =  req.facilities.split(",");
+        location.facilities = req.facilities.split(",");
         location.cootds = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
         location.openingTimes = [{
             days: req.body.days1,
             opening: req.body.opening1,
             closing: req.body.closing1,
             closed: req.body.closed1,
-        },{
+        }, {
             days: req.body.days2,
             opening: req.body.opening2,
             closing: req.body.closing2,
@@ -64,13 +72,13 @@ module.exports.locationsUpdateOne = function (req, res) {
 
         const result = location.save();
 
-        if(result) {
+        if (result) {
             sendJsonResponse(res, 404, "location not found.");
         } else {
             sendJsonResponse(res, 200, location);
         }
     }
-    
+
 };
 
 module.exports.locationsDeleteOne = function (req, res) {
@@ -83,8 +91,8 @@ module.exports.locationsDeleteOne = function (req, res) {
 
     const location = Loc.findByIdAndDelete(req.params.locationid).exec();
 
-    if(!location) {
-        sendJsonResponse(res, 404, {"message": "Location can not be deleted."});
+    if (!location) {
+        sendJsonResponse(res, 404, { "message": "Location can not be deleted." });
         return;
     } else {
         sendJsonResponse(res, 204, nul);
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,15 @@
-/* GET 'home' page */
-module.exports.homelist = function(req, res) {
+var request = require('request');
+
+var apiOptions = {
+    server: "http://localhost:3000"
+};
+
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://production-server-userl-here.com";
+}
+
+var renderHomepage = function (req, res, responseBody) {
+    
     res.render('locations-list', {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
@@ -7,35 +17,39 @@ module.exports.homelist = function(req, res) {
             strapline: 'Find places to work with wifi near you!'
         },
         sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: [{
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '100m'
-            },{
-            name: 'Cafe Hero',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '200m'
-            },{
-            name: 'Burger Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 2,
-            facilities: ['Food', 'Premium wifi'],
-            distance: '250m'
-        }]
+        locations: responseBody
     });
+};
+/* GET 'home' page */
+module.exports.homelist = function (req, res) {
+    var requestOptions, path;
+    path = "/api/locations";
+
+    requestOptions ={
+        url: apiOptions.server + path,
+        method: "GET",
+        json: {},
+        qs: {
+            lng : -0.7992599,
+            lat  :  51.378091,
+            maxDistance: 20
+        }
+    };
+    request(
+        requestOptions,
+        function(err, response, body) {
+            renderHomepage(req, res, body);
+        }
+    );
 };
 
 /* GET 'Location info' page */
-module.exports.locationInfo = function(req, res) {
-    res.render('location-info', {title: 'Location info' });
+module.exports.locationInfo = function (req, res) {
+    res.render('location-info', { title: 'Location info' });
 };
 
 /* GET "Add review page" */
-module.exports.addReview = function(req, res) {
-    res.render('location-review-form', {title: 'Add review' });
+module.exports.addReview = function (req, res) {
+    res.render('location-review-form', { title: 'Add review' });
 };
 
