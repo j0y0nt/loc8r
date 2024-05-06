b14ad1d35dd580d33ab285d08bf97a0eb681fefc
j0y0nt <4onsocial@gmail.com>
2024-05-06 16:07:51 +0530

    Added error handling.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,4 +1,5 @@
 var request = require('request');
+const { response } = require('../../app');
 
 var apiOptions = {
     server: "http://localhost:3000"
@@ -9,7 +10,17 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 var renderHomepage = function (req, res, responseBody) {
-    
+    var message;
+
+    if(!(responseBody instanceof Array)) {
+        message = "API lookup error";
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = "No places found nearby";
+        }
+    }
+
     res.render('locations-list', {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
@@ -17,7 +28,8 @@ var renderHomepage = function (req, res, responseBody) {
             strapline: 'Find places to work with wifi near you!'
         },
         sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: responseBody
+        locations: responseBody,
+        message: message
     });
 };
 /* GET 'home' page */
@@ -25,22 +37,42 @@ module.exports.homelist = function (req, res) {
     var requestOptions, path;
     path = "/api/locations";
 
-    requestOptions ={
+    requestOptions = {
         url: apiOptions.server + path,
         method: "GET",
         json: {},
         qs: {
-            lng : -0.7992599,
-            lat  :  51.378091,
+            lng: -0.7992599,
+            lat: 51.378091,
             maxDistance: 20
         }
     };
     request(
         requestOptions,
-        function(err, response, body) {
-            renderHomepage(req, res, body);
+        function (err, response, body) {
+            var i, data;
+            data = body;
+            if(response.statusCode === 200 && data.length) {
+                for (i = 0; i < data.length; i++) {
+                    data[i].distance = _formatDistance(data[i].distance);
+                }
+            }
+            
+            renderHomepage(req, res, data);
         }
     );
+};
+
+var _formatDistance = function (distance) {
+    var numDistance, unit;
+    if (distance > 1) {
+        numDistance = parseFloat(distance).toFixed(1);
+        unit = 'km';
+    } else {
+        numDistance = parseInt(distance * 1000, 10);
+        unit = 'm';
+    }
+    return numDistance + unit;
 };
 
 /* GET 'Location info' page */
