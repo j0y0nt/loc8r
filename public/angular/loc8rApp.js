587f7583f77e27f0bdf715b5a9617d774ee4e03e
j0y0nt <4onsocial@gmail.com>
2024-05-08 08:04:38 +0530

    Add angular.

---
+++ public/angular/loc8rApp.js
@@ -0,0 +1,33 @@
+var locationListCtrl = function ($scope) {
+    $scope.data = {
+        locations: [{
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '0.296456',
+            _id: '5370a35f2536f6785f8dfb6a'
+        }, {
+            name: 'Costy',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 5,
+            facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
+            distance: '0.7865456',
+            _id: '5370a35f2536f6785f8dfb6a'
+        }]
+    };
+};
+
+var ratingStars = function () {
+    return {
+        scope: {
+            thisRating : '=rating'
+        },
+        templateUrl: '/angular/rating-stars.html'
+    };
+}
+
+angular
+    .module('loc8rApp', [])
+    .controller('locationListCtrl', locationListCtrl)
+    .directive('ratingStars', ratingStars);
