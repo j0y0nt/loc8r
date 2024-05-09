c92041aa5eca0919476c4220bed347e33fc7e077
j0y0nt <4onsocial@gmail.com>
2024-05-09 12:33:31 +0530

    Add about page.

--- app_client/app.js
+++ app_client/app.js
@@ -2,20 +2,25 @@
 
     angular.module('loc8rApp', ['ngRoute', 'ngSanitize']);
 
-    function config ($routeProvider, $locationProvider) {
+    function config($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'home/home.view.html',
                 controller: 'homeCtrl',
                 controllerAs: 'vm'
             })
-            .otherwise({redirectTo: '/'});
+            .when('/about', {
+                templateUrl: '/common/views/genericText.view.html',
+                controller: 'aboutCtrl',
+                controllerAs: 'vm'
+            })
+            .otherwise({ redirectTo: '/' });
 
-            $locationProvider.html5Mode(true);
-    };
+    $locationProvider.html5Mode(true);
+};
 
-    angular
-        .module('loc8rApp')
-        .config(['$routeProvider', '$locationProvider', config]);
+angular
+    .module('loc8rApp')
+    .config(['$routeProvider', '$locationProvider', config]);
 
-})();
+}) ();
